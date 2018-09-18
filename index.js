393900ceb3719e46217bea04c8f2167428629ffe
Jan Spencer Catalan <jancatalan2@gmail.com>
2018-09-18 02:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Susan"
-var height = 74
-var message = ` is ${height} inches tall`
+var Name = "Susan"
+var Height = 74
+var Message = ` is ${height} inches tall`
 
-module.exports = {name, height, message}
+module.exports = {Name, Height, Message}
